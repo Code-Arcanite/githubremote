a5b24656560a4325494e416c04520a4ed91fc6e9
Arcanite <san10000tor@gmail.com>
2022-01-06 20:33:05 -0400

    VERSION 1

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>
+MY FIST GIT	
+</title>
+<h1>
+Titulo
+</h1>
+
+</head>
+
+<body>
+<h3>
+Pagina N°1
+</h3>
+<p>
+Esta es la pagina numero 1 en cuanto al uso de GIT se refiere.
+</p>
+
+
+</body>
+</html>
